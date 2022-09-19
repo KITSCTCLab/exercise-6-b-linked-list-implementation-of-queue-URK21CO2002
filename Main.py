5c4454ca537e1c4535b248f8a8a42d2c0b7b8444
URK21CO2002 <110015064+URK21CO2002@users.noreply.github.com>
2022-09-19 18:14:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -36,3 +36,33 @@ class Queue:
     """
     Removes node from the rear of the queue
     """
+    if not self.head is None:
+      self.head = self.head.next
+      if self.head is None:
+        self.tail = None
+
+  def status(self) -> None:
+    """
+    It prints all the elements of Queue.
+    """
+    elements = ""
+    curr = self.head
+    while not curr is None:
+      elements += str(curr.data) + "=>"
+      curr = curr.next
+    print(elements + "None")
+
+
+# Do not change the following code
+queue = Queue()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
