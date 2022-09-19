3ac24b7fd08b5818cdad06a53d9dff1450ddc980
URK21CO2002 <110015064+URK21CO2002@users.noreply.github.com>
2022-09-19 18:12:51 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,38 @@
 class Node:
+  """
+  Class implementing node of the LinkedList
+  Attributes:
+    -> data - data held by the node
+    -> next - link to the next node
+  """
   def __init__(self, data):
     self.data = data
     self.next = None
 
 
 class Queue:
+  """
+  Class implementing Queue as a LinkedList
+  """
   def __init__(self):
+    """
+    Initialises Queue object with pointers head and tail set to None
+    """
     self.head = None
-    self.last = None
+    self.tail = None
 
   def enqueue(self, data) -> None:
-    # Write your code here
+    """
+    Adds node containing data passed to the rear of the queue
+    """
+    new = Node(data)
+    if not self.tail is None:
+      self.tail.next = new
+    if self.head is None:
+      self.head = new
+    self.tail = new
 
   def dequeue(self) -> None:
-    # Write your code here
-
-  def status(self) -> None:
-    # Write your code here
-
-
-# Do not change the following code
-queue = Queue()
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = input_data.split(',')
-for i in range(len(operations)):
-  if operations[i] == "enqueue":
-    queue.enqueue(int(data[i]))
-  elif operations[i] == "dequeue":
-    queue.dequeue()
-queue.status()
+    """
+    Removes node from the rear of the queue
+    """
